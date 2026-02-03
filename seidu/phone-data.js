12c8523fb3c8fe2aa8a401012bca4983c3ece1dd
paul-shannon <paul.thurmond.shannon@gmail.com>
2026-02-03 07:23:13 -0800

    first demo for seidu

---
+++ seidu/phone-data.js
@@ -0,0 +1,19 @@
+var phones = [{name: 'iPhone SE', cost: 300},
+              {name: 'iPhone XR', cost: 400},
+              {name: 'Pixel 7', cost: 500},
+              {name: 'Samsung Galaxy S8+', cost: 900},
+              {name: 'Galazy Z Fold 5', cost: 1200},
+             ];
+
+/*******
+iPhone 12 Pro, 700
+iPhone 14 Pro Max, 800
+Pixel 7, 900
+Samsung Galaxy S8+, 1000
+Samsung Galaxy S20 Ultra, 1100 
+Galaxy Z Fold 5, 1200
+Samsung Galaxy A51/71, 1300
+Nest Hub, 1400
+Nest Hub Max, 1500
+
+***/
